04fbd310eeede9d0f8857bf64d35021457459d95
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-08-18 16:19:54 +0300

    Add FontAwesome Solid icons

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from "./router";
 import "./registerServiceWorker";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faGithub, faLinkedin);
+library.add(faGithub, faLinkedin, faEye);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
